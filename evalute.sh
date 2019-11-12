4dc2419884dac1f256fc334ddd144d7edb904530
DavidYo <lturtsamuel@gmail.com>
2019-11-12 21:15:12 +0800

    找回失落的實驗測資@@

--- evalute.sh
+++ evalute.sh
@@ -1,2 +1,2 @@
 cargo build --release || { echo "編譯失敗" ; exit 1; }
-./target/release/adams_leaf $1 exp_graph.json exp_flow_mid.json exp_flow_light.json $2
+./target/release/adams_leaf $1 exp_graph.json exp_flow_heavy.json exp_flow_heavy.json 1
