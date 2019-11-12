9999466ec6e7d02ce335087e8f21a059a0b11d8d
DavidYo <lturtsamuel@gmail.com>
2019-11-12 22:12:50 +0800

    完成實驗重現的文件

--- evalute.sh
+++ evalute.sh
@@ -1,2 +1,2 @@
 cargo build --release || { echo "編譯失敗" ; exit 1; }
-./target/release/adams_leaf $1 exp_graph.json exp_flow_heavy.json exp_flow_heavy.json 1
+./target/release/adams_leaf $1 exp_graph.json exp_flow_heavy.json exp_flow_reconf.json 4
